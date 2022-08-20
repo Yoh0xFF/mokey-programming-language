f9d9a3b58e6ad715f53f83f7a9df0b4c7d36d402
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-08-20 09:45:40 +0100

    Refactoring.

--- code/code.go
+++ code/code.go
@@ -6,6 +6,13 @@ import (
 	"fmt"
 )
 
+type Opcode byte
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
 type Instructions []byte
 
 func (inst Instructions) String() string {
@@ -45,96 +52,6 @@ func (inst Instructions) fmtInstruction(def *Definition, operands []int) string 
 	}
 
 	return fmt.Sprintf("ERROR: upnhandled operand count for %s\n", def.Name)
-}
-
-type Opcode byte
-
-const (
-	OpConstant Opcode = iota
-
-	OpAdd
-
-	OpPop
-
-	OpSub
-	OpMul
-	OpDiv
-
-	OpTrue
-	OpFalse
-
-	OpEqual
-	OpNotEqual
-	OpGreaterThan
-
-	OpMinus
-	OpBang
-
-	OpJumpNotTruthy
-	OpJump
-
-	OpNull
-
-	OpGetGlobal
-	OpSetGlobal
-
-	OpArray
-	OpHash
-	OpIndex
-
-	OpCall
-
-	OpReturnValue
-	OpReturn
-
-	OpGetLocal
-	OpSetLocal
-)
-
-type Definition struct {
-	Name          string
-	OperandWidths []int
-}
-
-var definitions = map[Opcode]*Definition{
-	OpConstant: {"OpConstant", []int{2}},
-	OpAdd:      {"OpAdd", []int{}},
-
-	OpPop: {"OpPop", []int{}},
-
-	OpSub: {"OpSub", []int{}},
-	OpMul: {"OpMul", []int{}},
-	OpDiv: {"OpDiv", []int{}},
-
-	OpTrue:  {"OpTrue", []int{}},
-	OpFalse: {"OpFalse", []int{}},
-
-	OpEqual:       {"OpEqual", []int{}},
-	OpNotEqual:    {"OpNotEqual", []int{}},
-	OpGreaterThan: {"OpGreaterThan", []int{}},
-
-	OpMinus: {"OpMinus", []int{}},
-	OpBang:  {"OpBang", []int{}},
-
-	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
-	OpJump:          {"OpJump", []int{2}},
-
-	OpNull: {"OpNull", []int{}},
-
-	OpGetGlobal: {"OpGetGlobal", []int{2}},
-	OpSetGlobal: {"OpSetGlobal", []int{2}},
-
-	OpArray: {"OpArray", []int{2}},
-	OpHash:  {"OpHash", []int{2}},
-	OpIndex: {"OpIndex", []int{}},
-
-	OpCall: {"OpCall", []int{1}},
-
-	OpReturnValue: {"OpReturnValue", []int{}},
-	OpReturn:      {"OpReturn", []int{}},
-
-	OpGetLocal: {"OpGetLocal", []int{1}},
-	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
