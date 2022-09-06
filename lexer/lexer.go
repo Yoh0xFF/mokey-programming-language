61649e4d5aa46174ff3f67a13ca5e9f5418b290e
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-09-06 01:18:19 +0100

    Tiny fix.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,7 +6,7 @@ type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
 	readPosition int  // current reading position in input (after current char)
-	ch           byte // current char ander examination
+	ch           byte // current char under examination
 }
 
 func New(input string) *Lexer {
